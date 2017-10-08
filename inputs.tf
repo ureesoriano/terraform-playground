1c47a5ee1c67dadd95bf3a5353913c8a030d84fc
Oriol Soriano <oriol.soriano@capside.com>
2017-10-08 10:32:51 +0000

    Move credentials to variables

---
+++ inputs.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+    description = "The AWS region to deploy resources in."
+    default = "eu-west-2"
+}
+
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = "eu-west-2"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
 }
 
 resource "aws_instance" "example" {
