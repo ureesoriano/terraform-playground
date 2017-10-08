662f1f9b4f9b3b3570f534e6e4c8370f9a63ba58
Oriol Soriano <oriol.soriano@capside.com>
2017-10-08 11:11:33 +0000

    Command to manage state from s3 bucket

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_key_pair" "acme" {
 }
 
 resource "aws_s3_bucket" "acme" {
-    bucket = "acme-terraform-101-images"
+    bucket = "terraform-playground-osoriano-images"
     acl    = "public-read"
 }
 
